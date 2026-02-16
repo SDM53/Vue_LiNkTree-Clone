6da0095be88a576d5cfe8f35f6dad7cd3567fd0a
SMorfin <sdmorfin53@gmail.com>
2026-02-15 21:43:23 -0600

    New Layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex min-h-screen flex-col">
+  <div class="min-h-screen bg-linear-to-bl from-yellow-200 via-red-400 to-purple-400">
     <RouterView />
   </div>
 </template>
